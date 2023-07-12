dba6e87e0feb242a8ddffcb9aa114e54fd3d628b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-12 16:38:40 +0300

    added test.js and test.ts to src

---
+++ src/test.js
@@ -0,0 +1,4 @@
+var firstName = "John";
+var lastName = "Doe";
+var fullName = "".concat(firstName, " ").concat(lastName);
+console.log("".concat(fullName, " is a great person!"));
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+const firstName: string = "John";
+const lastName: string = "Doe";
+const fullName: string = `${firstName} ${lastName}`;
+
+console.log(`${fullName} is a great person!`);
