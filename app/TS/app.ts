ff8306fd6373c9dbb21fa4e9a749a34ded402444
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-01 21:06:33 +0300

    update app.ts

--- app/TS/app.ts
+++ app/TS/app.ts
@@ -10,3 +10,12 @@ const returnString = (): string => {
 const mult = (num: number): number => {
   return num * 2;
 };
+
+const sum = (num1: number, num2: number, num3?: number): number => {
+  return num1 + num2;
+};
+sum(1, 2);
+
+const func = (user: { username: string; age: number; phone?: string }) => {
+  console.log(user.username);
+};
