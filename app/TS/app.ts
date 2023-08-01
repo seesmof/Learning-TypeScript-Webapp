9e2005974884286cea575f152a5d0be07ebd3017
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-01 21:03:36 +0300

    update app.ts

--- app/TS/app.ts
+++ app/TS/app.ts
@@ -1,35 +1,9 @@
-let user = {
-  username: "John",
-  age: 22,
-  isAdmin: false,
-};
-user.username = "Jane";
-user.age = 23;
-user.isAdmin = true;
-console.log(user);
+let test;
 
-let userObj: {
-  username: string;
-  age: number;
-  isAdmin: boolean;
-};
+test = 12;
+test = "121";
+test = true;
+test = [test];
 
-userObj = {
-  username: "John",
-  age: 22,
-  isAdmin: false,
-};
-
-let userObj2: {
-  username: string;
-  age: number;
-  isAdmin: boolean;
-  phone?: string;
-};
-
-userObj2 = {
-  username: "John",
-  age: 22,
-  isAdmin: false,
-  phone: "1234567890",
-};
+let testAnyArr: any[];
+testAnyArr = [1, "tw", true];
