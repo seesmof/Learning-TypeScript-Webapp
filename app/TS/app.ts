adb14b80775a3b8770986adc5730c6962d247647
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-01 21:02:44 +0300

    update app.ts

--- app/TS/app.ts
+++ app/TS/app.ts
@@ -19,3 +19,17 @@ userObj = {
   age: 22,
   isAdmin: false,
 };
+
+let userObj2: {
+  username: string;
+  age: number;
+  isAdmin: boolean;
+  phone?: string;
+};
+
+userObj2 = {
+  username: "John",
+  age: 22,
+  isAdmin: false,
+  phone: "1234567890",
+};
