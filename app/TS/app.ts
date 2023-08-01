246fe0896259aa19f80e9b81a534ddf8bd9d5704
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-01 18:22:34 +0000

    🌿: Tuesday, August 1, 2023 at 9:22:34 PM Eastern European Summer Time

--- app/TS/app.ts
+++ app/TS/app.ts
@@ -1,3 +1,55 @@
+const sayHi = () => {
+  console.log("hi");
+};
+
+const returnString = (): string => {
+  console.log("hi");
+  return "hi";
+};
+
+const mult = (num: number): number => {
+  return num * 2;
+};
+
+const sum = (num1: number, num2: number, num3?: number): number => {
+  return num1 + num2;
+};
+sum(1, 2);
+
+const func = (user: { username: string; age: number; phone?: string }) => {
+  console.log(user.username);
+};
+
+type UserType = {
+  username: string;
+  age: number;
+  phone?: string;
+};
+
+const betterOne = (user: UserType) => {
+  console.log(user.username);
+};
+
+type MyFunc = (a: number, b: string) => void;
+
+const write: MyFunc = (a, b) => {
+  console.log(`${a} times ${b}`);
+};
+
+type UserTwo = {
+  username: string;
+  age: number;
+  phone?: string;
+  theme: "dark" | "light";
+};
+
+const userTwo: UserTwo = {
+  username: "abc",
+  age: 1,
+  phone: "123",
+  theme: "light",
+};
+
 interface IUser {
   username: string;
   email: string;
