0068bdc7d7806e5ae7fff630a9aac1767c8f4263
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-02 10:07:17 +0300

    delete 1 file and update 1 file

---
+++ app/Posts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import List from "../components/post/List";
+
+const Posts = () => {
+  return (
+    <>
+      <List />
+    </>
+  );
+};
+
+export default Posts;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import List from "./components/post/List";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -6,10 +8,10 @@ export default function Home() {
       <h1 className="font-medium text-xl">
         Hi, <span className="text-blue-600">TypeScript</span>
       </h1>
-      <h2 className="font-medium">
+      <Link href={"/posts"} className="font-medium flex flex-row items-center">
+        <FaExternalLinkAlt className="mr-2" />
         Fetching Data from <span className="text-blue-600">API</span>
-      </h2>
-      <List />
+      </Link>
     </div>
   );
 }
