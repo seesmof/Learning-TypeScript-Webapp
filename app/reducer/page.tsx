c4cfb438c06a38a97555cbf0a0e0778893db384b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-03 17:09:54 +0300

    update page.tsx

--- app/reducer/page.tsx
+++ app/reducer/page.tsx
@@ -2,42 +2,28 @@
 import MainContainer from "@/components/MainContainer";
 import React, { useReducer } from "react";
 
+interface State {
+  theme: string;
+  fontSize: number;
+}
+
+interface Action {}
+
 const initialState = {
-  count: 0,
+  theme: "dark",
+  fontSize: 16,
 };
 
-const reducer = (state: any, action: any) => {
+const reducer = (state, action) => {
   switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      return state;
   }
 };
 
 const ReducerContextDemo = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
     <>
       <MainContainer>
-        <div className="flex flex-row gap-4 items-center">
-          <button
-            onClick={() => dispatch({ type: "decrement" })}
-            className="bg-blue-500 hover:bg-blue-600 text-white p-1.5 px-4 rounded-md"
-          >
-            -
-          </button>
-          <div className="font-medium text-xl">{state.count}</div>
-          <button
-            onClick={() => dispatch({ type: "increment" })}
-            className="bg-blue-500 hover:bg-blue-600 text-white p-1.5 px-4 rounded-md"
-          >
-            +
-          </button>
-        </div>
+        <div className="flex">Huh?</div>
       </MainContainer>
     </>
   );
